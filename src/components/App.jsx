00be59cdbace5670f1bac4cdc88edb9810967c39
mikus151 <mikus151@wp.pl>
2023-08-13 21:36:24 +0200

    task 3 first-v

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,9 @@
 import { Profile } from './Profile/index';
 import { Statistics } from './Statistics/index';
+import { FriendList } from './Friend-list/friend-list';
 import user from '../data/user.json';
 import data from '../data/data.json';
+import friends from '../data/friends.json';
 
 
 
@@ -25,8 +27,8 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
-
       <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
     </div>
   );
 };
