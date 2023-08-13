b891a27a5013a9d3838640006e57cd0c0e4aaa84
mikus151 <mikus151@wp.pl>
2023-08-13 18:04:45 +0200

    Update statistics.jsx

--- src/components/Statistics/statistics.jsx
+++ src/components/Statistics/statistics.jsx
@@ -1,15 +1,16 @@
 
 
 export const Statistics = ({ stats, title }) => (
-    
-  <ul>
-    {stats.map(stat => (
-        <li class="item" key={stat.id} >
-        
-            <span class="label">{stat.label}</span>
-        <span class="percentage"> {stat.percentage}%</span>
-      </li>
-    ))}
-  </ul>
+  <div>
+    <h2>{title}</h2>
+    <ul>
+      {stats.map(stat => (
+        <li class="item" key={stat.id}>
+          <span class="label">{stat.label}</span>
+          <span class="percentage"> {stat.percentage}%</span>
+        </li>
+      ))}
+    </ul>
+  </div>
 );
     
