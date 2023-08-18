637da072fe5475e769c50dc030cb3a2782a1bc68
mikus151 <mikus151@wp.pl>
2023-08-18 21:40:06 +0200

    css task 3

--- src/components/Friend-list/friend-list.jsx
+++ src/components/Friend-list/friend-list.jsx
@@ -1,10 +1,34 @@
-export const FriendList = ({ friends }) => 
-    <div>
+import css from './friend-list.module.css';
+
+export const FriendList = ({ friends }) => {
+  return (
+    <div >
+      <ul className={css.friendList}>
         {friends.map(friend => (
-            <li class="item" key={friend.id}>
-                <span class="status">{friend.isOnline}</span>
-                <img class="avatar" src={friend.avatar} alt="User avatar" width="48" />
-                <p class="name">{friend.name}</p>
-            </li>
+          <FriendListItem
+            key={friend.id}
+            avatar={friend.avatar}
+            name={friend.name}
+            isOnline={friend.isOnline}
+          />
         ))}
+      </ul>
     </div>
+  );
+};
+
+const FriendListItem = ({ avatar, name, isOnline }) => {
+  return (
+    <li className={css.itemFriends}>
+      <span
+        className={
+          isOnline
+            ? `${css.status} ${css.online}`
+            : `${css.status} ${css.offline}`
+        }
+      ></span>
+      <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
+      <p className={css.name}>{name}</p>
+    </li>
+  );
+};
