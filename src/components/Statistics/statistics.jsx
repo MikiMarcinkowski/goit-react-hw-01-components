7558807172a8976ea0bf956fc5b0f00c614227c6
mikus151 <mikus151@wp.pl>
2023-08-18 20:12:33 +0200

    css for task 2

--- src/components/Statistics/statistics.jsx
+++ src/components/Statistics/statistics.jsx
@@ -1,16 +1,26 @@
+import css from './statistics.module.css';
 
+export const Statistics = ({ stats, title }) => {
+  return (
+    <div className={css.statistics}>
+      <h2 className={css.statsTitle}>{title}</h2>
+      <ul className={css.statsList}>
+        {stats.map(stat => (
+          <li className={css.item} key={stat.id}>
+            <span className={css.label}>{stat.label}</span>
+            <span className={css.percentage}> {stat.percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-export const Statistics = ({ stats, title }) => (
-  <div>
-    <h2>{title}</h2>
-    <ul>
-      {stats.map(stat => (
-        <li class="item" key={stat.id}>
-          <span class="label">{stat.label}</span>
-          <span class="percentage"> {stat.percentage}%</span>
-        </li>
-      ))}
-    </ul>
-  </div>
-);
-    
+// const liItem = document.querySelector('.item');
+// liItem.style.backgroundColor = '#';
+// const setBg = () => {
+//   const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+// liItem.style.backgroundColor = '#' + randomColor;
+ 
+// };
+//  setBg();
